a15b9c27b85f48938d9539982a10337b7a92dd61
nehhhhha6 <n0370525@gmail.com>
2025-02-11 13:04:28 +0530

    update

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { PDFDocument } from "pdf-lib";
 import { Document } from "langchain/document";
+import { writeFile, unlink } from "fs/promises";
+import { UnstructuredLoader } from "langchain/document_loaders/fs/unstructured";
 
 async function deletePages(
   pdf: Buffer,
@@ -23,7 +25,13 @@ async function loadPdfFromUrl(url: string): Promise<Buffer> {
   return response.data;
 }
 
-async function convertPdfToDocuments(pdf: Buffer): Promise<Array<Document>> {}
+async function convertPdfToDocuments(pdf: Buffer): Promise<Array<Document>> {
+  if (!process.env.UNSTRUCTURED_API_KEY) {
+    throw new Error("Missing UNSTRUCTURED_API_KEY API key.");
+  }
+
+  const randomName = Math.random().toString(36).substring(7);
+}
 
 async function main({
   paperUrl,
