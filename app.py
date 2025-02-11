a09e72d8fccd6343fa81287d2cf9e9002b2291e8
nehhhhha6 <n0370525@gmail.com>
2025-02-11 15:09:36 +0530

    uddate

---
+++ app.py
@@ -0,0 +1,5 @@
+import os 
+import streamlit as st
+from langchain_groq import ChatGroq
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
